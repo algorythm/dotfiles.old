7c30662f7a01fca78eccf44c57124f5b56277704
awo-dev <anders@wiberg.tech>
2018-04-20 12:25:12 +0200

    Move linking of files to separate script

---
+++ link_files.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+ln -s ./linkfiles/gitconfig ~/.gitconfig
+ln -s ./linkfiles/vimrc.basic ~/.vimrc
+
--- source_files.sh
+++ source_files.sh
@@ -4,6 +4,3 @@ source ./shell/aliases.sh
 source ./shell/bash_prompt.sh
 source ./shell/exports.sh
 
-ln -s ./linkfiles/gitconfig ~/.gitconfig
-ln -s ./linkfiles/vimrc.basic ~/.vimrc
-
