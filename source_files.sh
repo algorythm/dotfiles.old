ffbd908eabe2f0d205a08fa5706c644101de3860
awo-dev <anders@wiberg.tech>
2018-04-20 12:21:19 +0200

    Simple install script

--- source_files.sh
+++ source_files.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+source ./shell/aliases.sh
+source ./shell/bash_prompt.sh
+source ./shell/exports.sh
+
+ln -s ./linkfiles/gitconfig ~/.gitconfig
+ln -s ./linkfiles/vimrc.basic ~/.vimrc
 
