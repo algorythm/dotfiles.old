d729d956080e403b92a8206a78da842a37cd01bb
awo-dev <anders@wiberg.tech>
2018-07-16 11:04:40 +0200

    Add export to bin folder

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,3 +1,4 @@
 # TODO: check if mac and that android studio is installed
 #export ANDROID_HOME=/Users/$USER/Library/Android/sdk
 #export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+export PATH=${PATH}:~/dotfiles/bin
