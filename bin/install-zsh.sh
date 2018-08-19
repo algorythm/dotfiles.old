ef50005b621223314b876339c33ea472c340a90f
algorythm <anders@wiberg.tech>
2018-08-19 22:14:13 +0200

    Set ZSH as default when executing install-zsh.sh

--- bin/install-zsh.sh
+++ bin/install-zsh.sh
@@ -10,6 +10,8 @@ echo "Installing ZSH..."
 [[ -x "$(command -v brew)" ]] && brew install zsh
 [[ -x "$(command -v apt-get)" ]] && sudo apt-get install -y zsh
 
+sudo -s 'echo $(which zsh) >> /etc/shells' && chsh -s $(which zsh)
+
 echo "Installing oh-my-zsh..."
 
 [[ -x "$(command -v curl)" ]] && sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
